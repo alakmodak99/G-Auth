c8428eeae1c69ee8d11e54a1f2d48ad68d9b4508
unknown <alakmodak99@gmail.com>
2022-09-03 23:29:40 +0530

    G-login

--- Config/db.js
+++ Config/db.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const mongoose = require("mongoose")
 //console.log(process.env.DB_URL)
 const connect =  async ()=>{
-    return await mongoose.connect(process.env.DB_URL)
+    return await mongoose.connect("mongodb+srv://gym-app-b:gym-app-b@cluster0.cpp908y.mongodb.net/?retryWrites=true&w=majority")
 }
 module.exports= connect
